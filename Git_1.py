f72a876e5c9eacec2e253960e827dea563114c9d
RenataAlexBoychenko <118833786+RenataAlexandrBoichenko@users.noreply.github.com>
2023-09-15 10:40:06 +0300

    Update Git_1.py Update

---
+++ Git_1.py
@@ -0,0 +1,6 @@
+class Person:
+
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age   #__age - скрытый и вывод будет _Person__age:
