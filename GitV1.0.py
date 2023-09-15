22621ac63f09b8f5ee133ce3f2b08133cb687d05
RenataAlexBoychenko <118833786+RenataAlexandrBoichenko@users.noreply.github.com>
2023-09-15 11:11:26 +0300

    Update GitV1.0.py Change

--- GitV1.0.py
+++ GitV1.0.py
@@ -1,3 +1,6 @@
-a = 5
-b = 6
-print(a * b)
+class Person:
+
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age   #__age - скрытый и вывод будет _Person__age:
