0453494dcc15ce36e8d102adb8675e9430dbf22e
Renata Boichenko <renataboychenko@gmail.com>
2023-10-10 15:01:26 +0300

    helpers

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture(scope="function")
 def driver():
